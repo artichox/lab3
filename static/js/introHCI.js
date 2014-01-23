dcabed730445b37f78a21c75853c4033d1bfccca
Lucy Richards <lucyanne@stanford.edu>
2014-01-22 22:45:47 -0800

    javascript lab yeehoo

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,42 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$('#testjs').text("TS Eliot is my main man");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+	$("a.thumbnail").click(projectClick);
+	$("#submitBtn").click(updateProject); 
+}
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).toggleClass("active");
+
+    var containingProject = $(this).closest(".project");	
+	var description = $(containingProject).find(".project-description");
+    if (description.length == 0) { 
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+    } 
+
+    if($(description).is(":visible"))
+    	description.fadeOut();
+    else
+    	description.fadeIn();
+}
+
+function updateProject(e) {
+   var projectID = $('#project').val();
+   $(projectID).animate({
+      width: $('#width').val()
+   });
+
+   var newText = $('#description').val();
+   $(projectID + " .project-description").text(newText);
 }
